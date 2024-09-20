557d68ffd6f26a54534b74585c8b90adee8c13d8
James Larsen <ljamesb@byu.edu>
2024-09-20 11:05:40 -0400

    outline of simple MPI commands

--- template_comp.cpp
+++ template_comp.cpp
@@ -4,31 +4,45 @@ using std::cout;
 using std::endl;
 
 
+// Function definition matching specs
+double f(double x)
+{
+    double y = 2*x;
+    for (int i = 1; i <= 10; i++) {
+        y += x*cos(y+i) / pow(1.5,i);
+    }
+    return y;
+}
+
+
 int main(int argc, char **argv)
 {
 
-    // Initialize the MPI environment.
+    // Initialize the MPI environment, get number of processors and processor ID
     MPI_Init(&argc, &argv);
+    int P;
+    int ID;
+    MPI_Comm_size(MPI_COMM_WORLD, &P);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ID);
 
-    // Initialize A
+    // TODO: Initialize A
 
     // Use MPI_BARRIER then call MPI_WTIME on root process
+    MPI_BARRIER(MPI_COMM_WORLD);
+    if ID == 0:
+        start = MPI_WTIME();
 
     // Iterate 10 times
 
-    // Compute verification values, send to root process
+    // TODO: Compute verification values, send to root process
 
-    // Call MPI_WTIME on root process
-
-    // Print out elapsed time and verification
+    // Call MPI_WTIME on root process, print elapsed time and verification
+    if ID == 0:
+        end = MPI_WTIME();
+        cout << end-start << " seconds" << endl;
+        //TODO: print verification values
 
     // Finalize MPI
     MPI_Finalize();
     return 0;
-}
-
-
-double f(double x)
-{
-    
 }
