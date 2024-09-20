6b1bd4196cd5359cd74ddaeb3a60ff608b6a5815
James Larsen <ljamesb@byu.edu>
2024-09-20 17:02:26 -0400

    finish verification values

--- sandbox.cpp
+++ sandbox.cpp
@@ -24,28 +24,31 @@ int main(int argc, char **argv)
     // cout << 20 % int(ceil(20./3)) << endl;
     // cout << argv[1] << endl;
 
-    int m = 20, n = 20, P=3, ID = stoi(argv[1]);
-    int numrows = ceil(m/P), numcols=n;
-    if (ID < (m % int(ceil(m/P))) ) {
-        numrows++;
-    }
-    double myA[numrows][numcols];
-    for (int i = 0; i < numrows; i++) {
-        for (int j = 0; j < n; j++) {
-            double val_i = ID*ceil(m/P) + i;
+    // int m = 20, n = 20, P=3, ID = stoi(argv[1]);
+    // int numrows = ceil(m/P), numcols=n;
+    // if (ID < (m % int(ceil(m/P))) ) {
+    //     numrows++;
+    // }
+    // double myA[numrows][numcols];
+    // for (int i = 0; i < numrows; i++) {
+    //     for (int j = 0; j < n; j++) {
+    //         double val_i = ID*ceil(m/P) + i;
 
-            if (ID < (m % int(ceil(m/P))) ) {
-                val_i += ID;
-            } else {
-                val_i += m % int(ceil(m/P));
-            }
+    //         if (ID < (m % int(ceil(m/P))) ) {
+    //             val_i += ID;
+    //         } else {
+    //             val_i += m % int(ceil(m/P));
+    //         }
 
-            myA[i][j] = j*sin(val_i) + val_i*cos(j) + sqrt(val_i+j+1);
-            cout << "myA[" << i << "][" << j << "]\t" << myA[i][j] << endl;
-        }
-    }
-    cout << 3*sin(15) + 15*cos(3) + sqrt(15+3+1) << endl;
-    cout << numrows << endl;
-    cout << ceil(m/P) << endl;
-    cout << m % int(ceil(m/P)) << endl;
+    //         myA[i][j] = j*sin(val_i) + val_i*cos(j) + sqrt(val_i+j+1);
+    //         cout << "myA[" << i << "][" << j << "]\t" << myA[i][j] << endl;
+    //     }
+    // }
+    // cout << 3*sin(15) + 15*cos(3) + sqrt(15+3+1) << endl;
+    // cout << numrows << endl;
+    // cout << ceil(m/P) << endl;
+    // cout << m % int(ceil(m/P)) << endl;
+
+    double vals[2] = {0,1};
+    cout << vals[0] << endl;
 }
--- template_comp.cpp
+++ template_comp.cpp
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
 
 
     // Initialize A (horizontal strips)
+    // TODO for speedup: flip matrix if m > n (tall)
     int m = argv[1], n = argv[2];
     int numrows = ceil(m/P), numcols=n;
     if (ID < (m % int(ceil(m/P))) ) {
@@ -36,12 +37,14 @@ int main(int argc, char **argv)
         for (int j = 0; j < n; j++) {
             double val_i = ID*ceil(m/P) + i;
 
+            // increment val_i based on ID
             if (ID < (m % int(ceil(m/P))) ) {
                 val_i += ID;
             } else {
                 val_i += m % int(ceil(m/P));
             }
 
+            // set myA
             myA[i][j] = j*sin(val_i) + val_i*cos(j) + sqrt(val_i+j+1);
         }
     }
@@ -54,24 +57,26 @@ int main(int argc, char **argv)
         start = MPI_Wtime();
     }
 
-    // Iterate 10 times, FIXME: not actual iteration
-    double val1 = 0;
-    for (int i = 0; i < 10; i++) {
-        val1 += f(ID + i);
-    }
-    double val2 = pow(val1, 2);
+    // Iterate 10 times
+    // TODO
 
     // Compute verification values, send to root process, FIXME: verification values not correct
-    double verification1, verification2;
-    MPI_Reduce(&val1, &verification1, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&val2, &verification2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    double vals[2] = {0,0}; // first entry: sum of all entries, second entry: sum of squares
+    for (int i = 0; i < numrows; i++) {
+        for (int j = 0; j < numcols; j++) {
+            vals[0] += myA[i][j];
+            vals[1] += pow(myA[i][j],2);
+        }
+    }
+    double verifications[2];
+    MPI_Reduce(&vals, &verifications, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
     // Call MPI_WTIME on root process, print elapsed time and verification
     if (ID == 0) {
         end = MPI_Wtime();
         cout << end-start << " seconds" << endl;
-        cout << "Sum of entries of A (verification 1):\t\t" << verification1 << endl;
-        cout << "Sum of squares of entries of A (verification 2):\t" << verification2 << endl;
+        cout << "Sum of entries of A (verification 1):\t\t" << verifications[0] << endl;
+        cout << "Sum of squares of entries of A (verification 2):\t" << verifications[1] << endl;
     }
 
     // Finalize MPI
