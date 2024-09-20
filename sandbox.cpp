df21f272771a1c54f9b8675de5314f8a529f9dde
James Larsen <ljamesb@byu.edu>
2024-09-20 10:40:27 -0400

    created f in sandbox

--- sandbox.cpp
+++ sandbox.cpp
@@ -7,16 +7,14 @@ using std::endl;
 double f(double x)
 {
     double y = 2*x;
-    
     for (int i = 1; i <= 10; i++) {
         y += x*cos(y+i) / pow(1.5,i);
     }
-
     return y;
 }
 
 
 int main(int argc, char **argv)
 {
-    cout << f(3.2) << endl;
+    cout << f(1.) << endl;
 }
