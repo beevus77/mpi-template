382bf50bcf3b0b2d3b7be76944ef28466a06c226
James Larsen <ljamesb@byu.edu>
2024-09-20 14:52:47 -0400

    working on initialization of A

--- sandbox.cpp
+++ sandbox.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 using std::cout;
 using std::endl;
+using std::min;
+using std::vector;
 
 
 double f(double x)
@@ -16,5 +18,21 @@ double f(double x)
 
 int main(int argc, char **argv)
 {
-    cout << f(1.) << endl;
+    // cout << f(1.) << endl;
+    // cout << min(3.4, ceil(3.3)) << endl;
+    // cout << 20 % int(ceil(20./3)) << endl;
+
+    int m = 20, n = 20, P=3, ID = 2;
+    vector<vector<double>> myA;
+    if (ID == P-1) {
+        double myA[m % int(ceil(m/P))][n];
+    } else {
+        double myA[int(ceil(m/P))][n];
+    }
+    for (int i = ID*int(ceil(m/P)); i < min(m, (ID+1)*int(ceil(m/P))); i++) {
+        for (int j = 0; j < n; j++) {
+            myA[i][j] = j*sin(i) + i*cos(j) + sqrt(i+j+1);
+        }
+    }
+    cout << myA << endl;
 }
--- template_comp.cpp
+++ template_comp.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+using std::min;
 
 
 // Function definition matching specs
@@ -25,7 +26,18 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &P);
     MPI_Comm_rank(MPI_COMM_WORLD, &ID);
 
-    // TODO: Initialize A
+    // Initialize A (horizontal strips)
+    int m = 20, n = 20; // FIXME: need to specify m and n outside program, i.e., input
+    if (ID == P-1) {    // FIXME: very poor load balancing, last processor has much less work
+        double myA[m % int(ceil(m/P))][n];
+    } else {
+        double myA[int(ceil(m/P))][n];
+    }
+    for (int i = ID*int(ceil(m/P)); i < min(m, (ID+1)*ceil(m/P)); i++) {
+        for (int j = 0; j < n; j++) {
+            myA[i][j] = j*sin(i) + i*cos(j) + sqrt(i+j+1);
+        }
+    }
 
     // Use MPI_BARRIER then call MPI_WTIME on root process
     MPI_Barrier(MPI_COMM_WORLD);
@@ -43,8 +55,8 @@ int main(int argc, char **argv)
 
     // Compute verification values, send to root process, FIXME: verification values not correct
     double verification1, verification2;
-    MPI_Reduce(&val1, &verification1, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&val2, &verification2, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&val1, &verification1, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&val2, &verification2, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
     // Call MPI_WTIME on root process, print elapsed time and verification
     if (ID == 0) {
